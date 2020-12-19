fca64f2df89656745feb3f24228513f3d135adbd
Wolfram Manthey <github@wmanth.net>
2020-12-19 01:00:27 +0100

    Added repo content endpoint

--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -6,22 +6,32 @@ import * as Global from "../globals.js"
 
 export const repos = express.Router()
 
+function repoPath(id: string) {
+    return path.join(Global.REPO_HOME, Global.RepoInventory.getEntry(id).local)
+}
+
 // list all tags in the repository
 repos.get("/:id/tags", ( req, res, next ) => {
-    const repoPath = path.join(Global.REPO_HOME, Global.RepoInventory.getEntry(req.params.id).local)
-    Git.Repository.openBare(repoPath)
+    Git.Repository.openBare(repoPath(req.params.id))
     .then(repo => Git.Tag.list(repo))
-    .then(tags => { res.json(tags) })
-    .catch(reason => { next(reason) })
+    .then(tags => res.json(tags))
+    .catch(reason => next(reason))
 })
 
 // list all files in the repository for the given tag name
 repos.get( "/:id/lookup", ( req, res, next ) => {
-    const repoPath = path.join(Global.REPO_HOME, Global.RepoInventory.getEntry(req.params.id).local)
-    Git.Repository.openBare(repoPath)
+    Git.Repository.openBare(repoPath(req.params.id))
     .then(repo => repo.getReferenceCommit(`refs/tags/${req.query.tagName}`))
     .then(commit => commit.getTree())
     .then(tree => FileEntry.parseTree(tree))
     .then(fileEntries => res.json(fileEntries))
-    .catch(reason => { next(reason) })
+    .catch(reason => next(reason))
+})
+
+// list all files in the repository for the given tag name
+repos.get( "/:id/content", ( req, res, next ) => {
+    Git.Repository.openBare(repoPath(req.params.id))
+    .then(repo => repo.getBlob(req.query.sha as string))
+    .then(blob => res.send(blob.content()))
+    .catch(reason => next(reason))
 })
